31bf6fc9844b96459919800dcde6a633d37ed42e
DavidYong <yongcheeho@gmail.com>
2024-09-27 16:46:08 +0800

    added logo and favicon

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,21 +1,26 @@
 <script>
-    const paths = [
-        {name: "home", path:'#home'},
-        {name: "about me", path:'#resume'},
-        {name: "work", path:'#work'},
-        {name: "contact", path:'#contact'}
-    ]
+	import Icon from '@iconify/svelte';
+	const paths = [
+		{ name: 'home', path: '#home' },
+		{ name: 'about me', path: '#resume' },
+		{ name: 'work', path: '#work' },
+		{ name: 'contact', path: '#contact' }
+	];
 </script>
 
 <div class="fixed p-5 bg-white/50 shadow-sm z-[999] w-full">
-    <div class="w-full max-w-[1200px] mx-auto">
-        <div class="flex justify-end gap-8 font-semibold">
-            {#each paths as path}
-            <a class="uppercase hover:text-[#EBB884] transition-all duration-300" href={path.path}>
-                {path.name}
-            </a>
-            {/each}
-        </div>
-    </div>
+	<div class="w-full max-w-[1200px] mx-auto">
+		<div class="flex justify-between font-semibold">
+			<div class="text-primary">
+				<Icon icon="tabler:hexagon-letter-d-filled" width="2em" height="2em" />
+			</div>
+			<div class="flex gap-8">
+				{#each paths as path}
+					<a class="uppercase hover:text-[#EBB884] transition-all duration-300" href={path.path}>
+						{path.name}
+					</a>
+				{/each}
+			</div>
+		</div>
+	</div>
 </div>
-
